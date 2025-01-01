e72c61664c62013cdea9107a21afb6e28e064993
turtur0 <vanncleopatra@gmail.com>
2025-01-02 10:14:28 +1100

    Remove LinkedList module

--- hashmap.js
+++ hashmap.js
@@ -1,7 +1,12 @@
-if (index < 0 || index >= buckets.length) {
-    throw new Error("Trying to access index out of bounds.")
-}
+function hashObject(key, value, nextNode = null) {
+    const getKey = () => key;
+    const getValue = () => value;
+    const getNextNode = () => nextNode;
+    const setValue = (newValue) => {value = newValue};
+    const setNextNode = (newNode) => {nextNode = newNode}
 
+    return {getKey, getValue, getNextNode, setValue, setNextNode}
+}
 
 class HashMap {
     constructor() {
@@ -12,27 +17,38 @@ class HashMap {
     }
 
     hash(key) {
-        let hashCode = code;
-    
+        let hashCode = 0;
+
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
             hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = hashCode % this.capacity
         }
 
         return hashCode;
     }
 
     set(key, value) {
+        const index = this.hash(key);
+
         if (index < 0 || index >= this.buckets.length) {
             throw new Error("Trying to access index out of bounds.")
         }
 
-        let hashCode = hash(key);
-
         if (!this.buckets[key]) {
-            
+            const bucket = hashObject(key, value, null);
+            bucket.append({ key, value })
+            this.storedKeys += 1;
+            this.buckets[index] = bucket;
+        } else {
+            const myList = this.buckets[index];
+            const addSuccess = myList.append({ key, value });
+            if (addSuccess) {
+                this.storedKeys += 1;
+            }
         }
-
-
     }
 }
+
+const test = new HashMap()
+test.set('apple', 'red')
