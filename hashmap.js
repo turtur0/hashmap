cba6323da9dcea583315fde7a45f7f8ae0a80108
turtur0 <vanncleopatra@gmail.com>
2025-01-02 09:28:47 +1100

    Create hash function

--- hashmap.js
+++ hashmap.js
@@ -1,3 +1,15 @@
 if (index < 0 || index >= buckets.length) {
     throw new Error("Trying to access index out of bounds.")
 }
+
+
+function hash(key){
+    let hashCode = code;
+    
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+        hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+}
